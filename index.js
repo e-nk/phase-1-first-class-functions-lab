e826f13bb388960728e2c45a17787d62a944b5fa
e-nk <enocklagat747@gmail.com>
2022-11-26 10:20:03 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return [
+        drivers[0],
+        drivers[1]
+    ]
+}
+function returnLastTwoDrivers() {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return [drivers[2], drivers[3]]
+}
+  
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+function createFareMultiplier(){
+      return fareQuintupler
+}
+  
+function fareQuintupler(num) {
+    return num * num
+}
+  
+function fareDoubler(fare){
+    return fare * 2;
+  
+}
+  
+function fareTripler(fare){
+    return fare * 3;
+  
+}
+  
+  
+function selectDifferentDrivers(drivers, func){
+    return func(drivers)
+}
